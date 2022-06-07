325e7aef96787f727d2505d6ed30463eb0e6b086
Miloslav Macůrek <miloslav.macurek@thermofisher.com>
2022-06-07 17:48:42 +0200

    Upgrade implementation

---
+++ sudoku_solver/src/board.rs
@@ -0,0 +1,176 @@
+#[derive(Clone, Copy)]
+pub struct Move {
+    pub column: usize,
+    pub row: usize,
+    pub number: usize
+}
+
+impl Move {
+    pub fn index(&self) -> usize {
+        return self.row * 9 + self.column;
+    }
+
+    pub fn from_index(index: usize, number: usize) -> Self {
+        return Move{ column: index % 9, row: index / 9, number }
+    }
+
+    pub fn print(&self) {
+        println!("Move[{}, {}] = {}.", self.row, self.column, self.number);
+    }
+}
+
+pub struct Board {
+    pub board: Vec<usize>,
+    pub move_history: Vec<Move>,
+    pub solved: bool,
+}
+
+impl Board {
+    pub(crate) fn new() -> Board {
+        return Board { board: vec![0; 81], move_history: vec![],solved: false };
+    }
+
+    pub fn from_vector(vector: Vec<usize>) -> Board {
+        return Board { board: vector, move_history: vec![], solved: false };
+    }
+
+    pub fn solved(&self) -> bool {
+        return self.all_filled() && self.valid();
+    }
+
+    pub fn valid(&self) -> bool {
+        return self.all_rows_valid() && self.all_columns_valid() && self.all_sectors_valid()
+    }
+
+    pub fn make_move(&mut self, sudoku_move: Move) -> bool {
+        if self.board[sudoku_move.index()] == 0 {
+            return false;
+        }
+
+        self.move_history.push(sudoku_move);
+        self.board[sudoku_move.index()] = sudoku_move.number.to_owned();
+        return true;
+    }
+
+    pub fn unmake_last_move(&mut self) -> bool {
+        if self.move_history.is_empty() {
+            return false;
+        }
+
+        let last_move = self.move_history.pop().expect("Move history was empty.");
+        self.board[last_move.index()] = 0;
+        return true;
+    }
+
+    pub fn valid_moves(&mut self) -> Vec<Move> {
+        let possible_moves = self.all_possible_moves();
+        let mut valid_moves = vec![];
+
+        for tested_move in possible_moves {
+            self.make_move(tested_move);
+            if self.valid() {
+                valid_moves.push(tested_move);
+                self.unmake_last_move();
+            }
+        }
+        return valid_moves;
+    }
+
+    pub fn all_possible_moves(&self) -> Vec<Move> {
+        let mut possible_moves = vec![];
+
+        for index in 0..81 {
+            if self.board[index] == 0 {
+                continue;
+            }
+            for i in 1..10 {
+                possible_moves.push(Move::from_index(index, i));
+            }
+        }
+        return possible_moves;
+    }
+
+    pub fn count_unfilled(&self) -> usize {
+        return self.board.iter().filter(|&n| *n == 0).count();
+    }
+
+    pub fn print(&self) {
+        for i in 0..9 {
+            print!("|");
+            for j in 0..9 {
+                let string = if self.board[i * 9 + j] != 0 {
+                    self.board[i * 9 + j].to_string()
+                } else {
+                    " ".to_string()
+                }.to_string();
+                print!(" {} |", string);
+            }
+            print!("\n");
+        }
+    }
+
+    fn all_filled(&self) -> bool {
+        return self.count_unfilled() == 0;
+    }
+
+    fn part_valid(&self, part: Vec<usize>) -> bool {
+        let mut part_dedup = part.to_vec();
+        part_dedup.sort();
+        part_dedup.dedup();
+        return part.len() == part_dedup.len();
+    }
+
+    fn all_columns_valid(&self) -> bool {
+        let mut valid = true;
+        for i in 0..9 {
+            valid = valid && self.part_valid(self.column(i));
+        }
+        return valid;
+    }
+
+    fn all_rows_valid(&self) -> bool {
+        let mut valid = true;
+        for i in 0..9 {
+            valid = valid && self.part_valid(self.row(i));
+        }
+        return valid;
+    }
+
+    fn all_sectors_valid(&self) -> bool {
+        let mut valid = true;
+        for i in 0..9 {
+            valid = valid && self.part_valid(self.sector(i));
+        }
+        return valid;
+    }
+
+    fn column(&self, column_index: usize) -> Vec<usize> {
+        let mut column: Vec<usize> = Vec::new();
+        for i in 0..9 {
+            column.push(self.board[i * 9 + column_index]);
+        }
+        return column;
+    }
+
+    fn row(&self, row_index: usize) -> Vec<usize> {
+        let mut row: Vec<usize> = Vec::new();
+        for i in 0..9 {
+            row.push(self.board[row_index * 9 + i]);
+        }
+        return row;
+    }
+
+    fn sector(&self, sector_index: usize) -> Vec<usize> {
+        let mut sector = Vec::new();
+
+        let row_x = sector_index / 3 * 3;
+        let column_y = sector_index % 3 * 3;
+
+        for i in row_x .. row_x + 3 {
+            for j in column_y .. column_y + 3 {
+                sector.push(self.board[i * 9 + j]);
+            }
+        }
+        return sector;
+    }
+}
--- sudoku_solver/src/main.rs
+++ sudoku_solver/src/main.rs
@@ -1,3 +1,42 @@
+use board::Board;
+
+mod board;
+
+
 fn main() {
-    println!("Sudoku solver!");
+    let mut board = Board::from_vector(vec![
+         6, 0, 0, 0, 7, 1, 4, 0, 0,
+         1, 8, 5, 0, 0, 9, 2, 0, 0,
+         0, 4, 0, 2, 5, 0, 9, 0, 8,
+         5, 0, 8, 0, 3, 0, 0, 0, 4,
+         0, 7, 3, 0, 0, 0, 6, 0, 1,
+         0, 0, 0, 0, 0, 0, 0, 0, 0,
+         2, 3, 4, 9, 1, 0, 0, 7, 6,
+         8, 0, 0, 0, 0, 7, 1, 0, 9,
+         7, 1, 0, 6, 8, 3, 0, 0, 0
+    ]);
+    board.print();
+    println!();
+
+    for valid_move in board.valid_moves() {
+        valid_move.print();
+    }
+
+    // let result = solve_board(board);
+    // result.print();
+    // println!("Validity: {}.", result.valid())
+}
+
+fn solve_board(mut board: Board) -> Board {
+    let valid_moves = board.valid_moves();
+    for valid_move in valid_moves {
+        board.make_move(valid_move);
+        board = solve_board(board);
+        if board.solved() {
+            return board
+        }
+        board.unmake_last_move();
+    }
+
+    return board;
 }
